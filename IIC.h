6d1e6190b5101f7c062ba0005e6cd3d6a6febeb9
Mohammed-Mostafa <77737900+mohammed-mostafa2001@users.noreply.github.com>
2022-12-11 03:07:44 +0200

    Update IIC.h

--- IIC.h
+++ IIC.h
@@ -17,7 +17,7 @@
 #define REP_START_ACK            0x10 // repeated start
 #define SLAVE_ADD_AND_WR_ACK     0x18 // Master transmit ( slave address + Write request ) ACK
 #define SLAVE_ADD_AND_RD_ACK     0x40 // Master transmit ( slave address + Read request ) ACK
-#define WR_BYTE_ACK              0xI8 // Master transmit data ACK
+#define WR_BYTE_ACK              0x28 // Master transmit data ACK
 #define RD_BYTE_WITH_NACK        0x58// Master received data with not ACK
 #define RD_BYTE_WITH_ACK         0x50// Master received data with not ACK
 #define SLAVE_ADD_RCVD_RD_REQ    0xA8 // means that slave address is received with write req
